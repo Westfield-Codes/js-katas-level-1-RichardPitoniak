7868967a6a481792e07d08f872919e86d7931a31
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-02-14 15:13:37 +0000

    age kata

--- done.js
+++ done.js
@@ -17,7 +17,13 @@ alert("My favorite color is " + color);
 
 
 /* Prompt */
-
+/* Var Alert Prompt */
+// make an integer variable for age, prompt user for their age
+var age = prompt ("what is your age?");
+// make a string variable for name, prompt user for their name
+var aaaaa = prompt ("what's your name");
+// alert name is age years old
+alert (aaaaa + " is " + age + " years old");
 
 /* Prompt with Conditional */
 
