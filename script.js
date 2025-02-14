b56531be451fc1a40f9c1120ce757718b3ec95c5
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-02-14 15:54:46 +0000

    ice crean prompt done ( plus if/else )

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 /* Var Alert Prompt */
 // make a string variable for favorite ice cream flavor, prompt user for it
-var flavor = prompt("what is your favorite ice cream flavor?");
+var flavor = prompt ("What is your favorite ice cream flavor");
 // make an integer variable for number of scoops on the cone, prompt user for it
-var scoops = prompt ("how many scoops of that do you want?")
+var scoops = prompt ("how many scoops?")
 // alert "You want " scoops "scoops of " flavor
-alert ("you want " + scoops + " of " + flavor);
+
+if ( scoops > 3) {
+    alert ("that's too many");
+} else {
+    alert ("you want " + scoops + " of " + flavor);
+}
