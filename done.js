222de9b02fe3e66e847077c6a7edb9f1661e4cb8
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-02-25 15:49:16 +0000

    all prompt with conditionals done

--- done.js
+++ done.js
@@ -11,6 +11,11 @@ var color = prompt ("What is your favorite color?");
 alert (color + " is a nice color ");
 /* Prompt with Conditional */
 
+var userColor = prompt ("What is your favorite color? ");
+
+if (userColor == "black") alert ("Black isn't really a color");
+
+else alert (userColor + "is a nice color");
 
 /* AGE */
 
@@ -28,6 +33,13 @@ alert (aaaaa + " is " + age + " years old");
 
 /* Prompt with Conditional */
 
+var age = prompt ("How old are you?");
+
+var name = prompt ("What is your name");
+
+if (age > 16) alert ("you don't look that old");
+
+else alert (name + " is " + age + " years old" ); 
 
 /* ICE CREAM */
 
@@ -41,7 +53,13 @@ var scoops = prompt ("how many scoops of that do you want?")
 alert ("you want " + scoops + " of " + flavor);
 
 /* Prompt with Conditional */
+var flavor = prompt ("What is your favorite ice cream flavor?");
 
+var scoops = prompt ("How many scoops of that do you want?");
+
+if (scoops > 3) alert ("the max is 3 scoops");
+
+else alert("You want " + scoops + " of " + flavor);
 
 /* PET */
 
@@ -49,6 +67,24 @@ alert ("you want " + scoops + " of " + flavor);
 
 
 /* Prompt */
-
+/* Var Alert Prompt */
+// make a variable for a pet type, prompt user for it 
+var petType = prompt ("What type of animal is your pet?");
+// make a variable for a pet name, prompt user for it
+var petName = prompt ("What is it's name?");
+// alert "You have a pet type named pet name 
+alert ("you have a " + petType + " named " + petName);
 
 /* Prompt with Conditional */
+
+var petType = prompt ("what type of animal is your pet?")
+
+var petName = prompt ("what is its name?")
+
+alert ("you have a " + petType + " named " + petName )
+
+if (petType == "dog") alert ("I like dogs too");
+
+else if (petType == "cat") alert ("I'm allergic to cats");
+
+else alert ("What an interesting pet.");
--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-/* Var Alert Prompt */
-// make a variable for a pet type, prompt user for it 
-var petType = prompt ("What type of animal is your pet?");
-// make a variable for a pet name, prompt user for it
-var petName = prompt ("What is it's name?");
+/* Var Alert Prompt Conditionals */
+// make a variable for pet type, prompt user for it 
+var petType = prompt ("what type of animal is your pet?")
+// make a variable for pet name, prompt user for it
+var petName = prompt ("what is its name?")
 // alert "You have a pet type named pet name 
-alert ("you have a " + petType + " named " + petName);
+alert ("you have a " + petType + " named " + petName )
+// if pet is a dog, say "I like dogs, too!"
+if (petType == "dog") alert ("I like dogs too");
+// if it is a cat, say "I'm allergic to cats"
+else if (petType == "cat") alert ("I'm allergic to cats");
+// If it is not a dog or a cat, say "what an interesting pet!"
+else alert ("What an interesting pet.");
+
