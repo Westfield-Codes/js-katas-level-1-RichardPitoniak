b2489f8542921c96eea147ac4fba114bdabf21e3
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-02-25 15:17:07 +0000

    pet var alert done

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 /* Var Alert Prompt */
-// make a string variable for favorite ice cream flavor, prompt user for it
-var flavor = prompt ("What is your favorite ice cream flavor");
-// make an integer variable for number of scoops on the cone, prompt user for it
-var scoops = prompt ("how many scoops?")
-// alert "You want " scoops "scoops of " flavor
-
-if ( scoops > 3) {
-    alert ("that's too many");
-} else {
-    alert ("you want " + scoops + " of " + flavor);
-}
+// make a variable for a pet type, prompt user for it 
+var petType = prompt ("What type of animal is your pet?");
+// make a variable for a pet name, prompt user for it
+var petName = prompt ("What is it's name?");
+// alert "You have a pet type named pet name 
+alert ("you have a " + petType + " named " + petName);
